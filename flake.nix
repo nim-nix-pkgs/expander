aaea3c763a398858831ef4a0ce25ede1cfbe5600
hugosenari <hugosenari@gmail.com>
2022-03-12 02:09:12 -0300

    chore: re index expander

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Code expander for competitive programing in Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."expander-main".url = "path:./main";
+  inputs."expander-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."expander-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
